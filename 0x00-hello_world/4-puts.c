a35a1ed0123bf23e9eb2ba67404647227acb8b61
rachaelmaryanne <rachael.andati@yahoo.com>
2022-06-03 12:51:25 -0700

    coding

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+ /**
+  *main
+  *
+  * return always 0 (success)
+  *
+  */int main(void)
+  {
+	  puts("\"Programming is like building a multilingual puzzle");
+	  return (0);
+
+
+  }
+
+		
+

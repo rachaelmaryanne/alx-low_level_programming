da8eed6f0b8b637bad1aa99eaf9d2f1906ac1b2c
rachaelmaryanne <rachael.andati@yahoo.com>
2022-06-03 13:10:38 -0700

    coding

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,13 +1,11 @@
-#include <stdio.h>
+#include <stidio.h>
 /**
+ * main Entry - point
  *
- *main - Entry point
- *
- *Return: Always 0 (Success)
- *\
- int main(void)
- {
-   puts("\"Programming is like building a multilingual puzzle");
-   return (0);
- }
- 
+ * Return: Always 0 (Sucess)
+ */
+int main(void)
+{
+	puts("\"programming is like bulding amultiligual puzzle");
+	return(0);
+}

66541041f0d1f0ee63c56cbb1bb5fe7016738c4c
rachaelmaryanne <rachael.andati@yahoo.com>
2022-06-04 12:39:15 -0700

    commit

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,10 +1,21 @@
 #include <stdio.h>
 /**
- * main - Prints out a string to stdout.
+ * main - Prints out the sizes of various data types on the computer
+ * this program is compiled on.
  * Return: 0 if success.
  */
 int main(void)
 {
-	printf("with proper grammar, but the outcome is a piece of art,\n");
+	char c;
+	int i;
+	long l;
+	long long ll;
+	float f;
+
+	printf("Size of a char: %lu byte(s)\n", (unsigned long)sizeof(c));
+	printf("Size of an int: %lu byte(s)\n", (unsigned long)sizeof(i));
+	printf("Size of a long int: %lu byte(s)\n", (unsigned long)sizeof(l));
+	printf("Size of a long long int: %lu byte(s)\n", (unsigned long)sizeof(ll));
+	printf("Size of a float: %lu byte(s)\n", (unsigned long)sizeof(f));
 	return (0);
 }

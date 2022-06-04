e008359e8f7cfe57728b3ae18de9869e803c5822
rachaelmaryanne <rachael.andati@yahoo.com>
2022-06-04 12:26:32 -0700

    git

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,12 +1,10 @@
 #include <stdio.h>
-
 /**
- * main Entry - point
- *
- * Return: Always 0 (Sucess)
+ * Description: main - Prints out a string to stdout.
+ * Return: 0 if success.
  */
 int main(void)
 {
-	puts("\"programming is like bulding a multiligual puzzle");
-	return(0);
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
 }

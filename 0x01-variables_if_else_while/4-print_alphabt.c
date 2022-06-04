daca77d15969a87f45474c0f252c2cc4c53a9a1f
rachaelmaryanne <rachael.andati@yahoo.com>
2022-06-04 04:49:50 -0700

    code

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * main - print alphabet in lower case.
+(*
+ *Return: 0 on success
+ */
+int main(void)
+{
+	char a = 'c';
+	while (c <= 'z')
+	{
+		if (c != 'q' && c != 'e')
+		{
+			putchar(c);
+		}
+		c++;
+	}
+	putchar('\n')
+	return (0);
+}

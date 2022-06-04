fe5b8f7581642fcc3546e537de7c44461c5f0ffe
rachaelmaryanne <rachael.andati@yahoo.com>
2022-06-04 05:20:36 -0700

    my

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
 /**
- * main- prints numbers 0-9 without using char or printf/puts, and putchar 2x max
+ * main - prints numbers 0-9 without using char or printf/puts, and putchar 2x max
 (*
- *Return:0 on success
+ * Return: 0 on success
  */
 int main(void)
 {

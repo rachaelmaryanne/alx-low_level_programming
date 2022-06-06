746a90604cfa22bbd14f9323c15b49dc80113a64
rachaelmaryanne <rachael.andati@yahoo.com>
2022-06-06 11:38:34 -0700

    my commit

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -9,17 +9,18 @@
 int main(void)
 {
 	int d, p;
+
+	for (d = '0'; d < '9'; d++)
 	{
-		for (d = '0'; d < '9'; p++);
+		for (p = d + 1; p <= '9'; p++)
 		{
-			if ( p != d)
+			if (p != d)
 			{
 				putchar(d);
 				putchar(p);
-				
+
 				if (d == '8' && p == '9')
 					continue;
-
 				putchar(',');
 				putchar(' ');
 			}

104e04c98a79632e2d005884d20eb287d0e1ff3d
rachaelmaryanne <rachael.andati@yahoo.com>
2022-06-06 11:20:43 -0700

    my commit

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -2,31 +2,29 @@
 #include <stdlib.h>
 #include <time.h>
 /**
- * main -  prints all possible different combinations of two digits.
+ * main - print all possible different comdination of two digits.
 (*
  * Return: 0 on success
  */
 int main(void)
 {
 	int d, p;
+	{
+		for (d = '0'; d < '9'; p++);
+		{
+			if ( p != d)
+			{
+				putchar(d);
+				putchar(p);
+				
+				if (d == '8' && p == '9')
+					continue;
 
-	 for (d = '0'; d < '9'; d++)
-	 {
-		 for (p = d + 1; p <= '9'; p++)
-		 {
-			 if (p != d)
-			 {
-				 putchar(d);
-				 putchar(p);
-
-				 if (d == '8' && p == '9')
-					 continue;
-
-				 putchar(',');
-				 putchar(' ');
-			 }
-		 }
-	 }
-	 putchar('\n');
-	 return (0);
+				putchar(',');
+				putchar(' ');
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
 }

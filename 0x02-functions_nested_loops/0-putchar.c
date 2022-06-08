f520d739174b80b336329162fd23112907a085f7
rachaelmaryanne <rachael.andati@yahoo.com>
2022-06-08 03:40:45 -0700

    commit

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+/**
+ * main - print strings
+ *
+ *Return: Always
+ */
+int main(void)
+{
+	char a = '_';
+	char b = 'p';
+	char c = 'u';
+	char d = 't';
+	char e = 'c';
+	char f = 'h';
+	char g = 'a';
+	char h = 'r';
+
+	putchar(a);
+	putchar(b);
+	putchar(c);
+	putchar(d);
+	putchar(e);
+	putchar(f);
+	putchar(g);
+	putchar(h);
+	putchar('\n');
+	return (0);
+}

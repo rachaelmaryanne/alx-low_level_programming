3ab6427e7688138ccbc97bcf587a1badb05aaa90
rachaelmaryanne <rachael.andati@yahoo.com>
2022-06-13 12:30:29 -0700

    hard

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -7,14 +7,18 @@ void more_numbers(void);
 {
 	int i, j;
 
-	for (i = 0; i < 10; i++)
+	i = j = 0;
+	while (i < 10)
 	{
-		for (j = 0; j < 15; j++)
+		while (j <= 14)
 		{
 			if (j >= 10)
 				_putchar(j / 10 + '0');
 			_putchar(j % 10 + '0');
+			++j;
 		}
-		_putchar(j % 10 + '0');
+		_putchar('\n');
+		j = 0;
+		i++;
 	}
 }

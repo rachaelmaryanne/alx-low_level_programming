11e5af56a60e18d95875e1d24786f3d8c9c620da
rachaelmaryanne <rachael.andati@yahoo.com>
2022-06-15 12:37:28 -0700

    correction

--- 0x06-pointers_arrays_strings/100-rot13.c
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -13,7 +13,9 @@ void print_number(int n)
 	expo = 1;
 	/*Check negatives*/
 	if (n >= 0)
+	{
 		res = n * -1;
+	}
 	else
 	{
 		res = n;
@@ -26,10 +28,13 @@ void print_number(int n)
 	{
 		expo *= 10;
 		temp /= 10;
+		printf("expo: %d, temp: %d, orig: %d\n", expo, temp, res);
 	}
+
 	/*Main */
 	while (expo >= 1)
 	{
+		printf("expo: %d, res: %d\n", expo, ((res / expo) % 10) * -1);
 		_putchar(((res / expo) % 10) * -1 + '0');
 		expo /= 10;
 	}

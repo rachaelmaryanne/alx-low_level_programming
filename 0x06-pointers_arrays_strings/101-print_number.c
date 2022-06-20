506d8e0b6e2e72a316ac7c270f1dde9e334a5658
rachaelmaryanne <rachael.andati@yahoo.com>
2022-06-20 12:23:07 -0700

    function that prints an integer.

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -1,23 +1,27 @@
 #include <stdio.h>
-
-int main(void)
+#include "main.h"
+/**
+ * print_number - prints an integer using _putchar
+ * @n: integer to be printed
+ */
+void print_number(int n)
 {
-	 int n;
-	 int a[5];
-	 int *p;
+	unsigned int num;
 
-	 a[2] = 1024;
-	 p = &n;
-	 /*
-	  * write your line of code here...
-	  * Remember:
-	  *- you are not allowed to use a
-	  *- you are not allowed to modify 
-	  *- only one statement
-	  *- you are not allowed to code anything else than this line of code
-	  */
-	 *(p + 5) = 98;
-	 /* ...so that this prints 98\n */
-	 printf("a[2] = %d\n", a[2]);
-	 return (0);
+	/* print - infront of negative numbers */
+	if (n < 0)
+	{
+		_putchar('-');
+		num = -n;
+	}
+	else
+	{
+		num = n;
+	}
+	/* print the digits infront of the current digit */
+	if (num / 10)
+		print_number(num / 10);
+
+	/* print current digit */
+	_putchar((num % 10) + '0');
 }
